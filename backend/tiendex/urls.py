1f9741d5f8e240aac6fb96ce2d12fdf06776b1b0
Jose <jadanobregonb@gmail.com>
2025-03-12 15:03:44 -0600

    changed

--- backend/tiendex/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('inventory/', include('inventory.urls')),
-    path('general/', include('general.urls'))
-]
